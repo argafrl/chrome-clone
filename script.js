a215f48431010c06ef16a390ac73c8157f228923
argafrl <44842687+argafrl@users.noreply.github.com>
2023-05-11 14:11:35 +0700

    add search list history

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-var modal = document.getElementById('modalProfile');
+var modalProfile = document.getElementById('modalProfile');
+var btnProfile = document.getElementById('btnProfile');
 
-var btn = document.getElementById('btnProfile');
+let searchContainer = document.getElementById('search');
+let searchList = document.getElementById('search-list');
+let searchInput = document.getElementById('search-input');
+let searchInputTextbox = document.getElementById('search-input-textbox');
 
-btn.onclick = () => {
-  modal.style.display = 'block';
+btnProfile.onclick = () => {
+  modalProfile.style.display = 'block';
+};
+
+searchInputTextbox.onclick = () => {
+  searchList.style.display = 'flex';
+  searchInput.style.borderRadius = '10px 10px 0 0';
 };
 
 window.onclick = (event) => {
-  if (event.target == modal) {
-    modal.style.display = 'none';
+  if (event.target == modalProfile) {
+    modalProfile.style.display = 'none';
   }
 };
+
+document.addEventListener('click', function clickOutside(event) {
+  if (!searchContainer.contains(event.target)) {
+    searchList.style.display = 'none';
+    searchInput.style.borderRadius = '50px';
+  }
+});
